a5fc285fe8717e35d304797ae7838b1c201f164e
Ahmad Riaz <kashanali982982@gmail.com>
2025-08-07 01:47:22 +0500

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Route</title>
+</head>
+<body>
+      <h1>I am view here</h1>
+      <a href="{{route('dashboard')}}">click</a>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get("home" , function(){
+//     return "Helo Route";
+// });
+
+
+// Route::get("/wel" , function(){
+//     return view("home");
+// });
+
+
+// Route::get("/wel" , function(){
+//     return view("home");
+// })->name("dashboard");
+
+
+// route grouping
+Route::prefix("/admin")->group(function(){
+    Route::get("/dashboard" , function(){
+        return "Admin Dashboard";
+    });
+    Route::get("/user" , function(){
+        return "Admin User";
+    });
+
+});

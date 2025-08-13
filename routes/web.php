13577b67b74b72e060dabfe943cb1cea3fde1c0c
Ahmad Riaz <kashanali982982@gmail.com>
2025-08-13 12:44:23 +0500

    Studing parameter routes with our retrection

--- routes/web.php
+++ routes/web.php
@@ -18,23 +18,67 @@ use Illuminate\Support\Facades\Route;
 // syntax ----------------
 
 // calling simple route 
-Route::get('/home' , function(){
-    return 'welcome to home page';
-});
+// Route::get('/home' , function(){
+//     return 'welcome to home page';
+// });
 
 // calling view with route 
 
-Route::get('/laravel' , function(){
-    return view('home');
-});
+// Route::get('/laravel' , function(){
+//     return view('home');
+// });
 
 // short way to writing route
 
-Route::view('short' , 'home');
+// Route::view('short' , 'home');
 
 // other file  calling
 
-Route::view('new' , 'about');
+// Route::view('new' , 'about');
 
 
 
+// Topic No 2 : how to create Parameter routes 
+
+// if we not put parameter in route then it will throw error
+// Route::get('parameter/{id}' , function($id){
+//     return $id;
+// });
+
+// if we initialize blank variable then it will not throw error
+// Route::get('parameter/{id?}' , function($id = ''){
+//     return $id;
+// });
+
+// route with only numbers else 404 
+
+// Route::get('parameter/{id?}' , function($id = ''){
+//     return $id;
+// })->whereNumber('id');
+
+
+// route with alpha 
+
+// Route::get('parameter/{id?}' , function($id = ''){
+//     return $id;
+// })->whereAlpha('id');
+
+
+// route with alpha  numeric mean run for both case 
+
+// Route::get('parameter/{id?}' , function($id = ''){
+//     return $id;
+// })->whereAlphaNumeric('id');
+
+
+// for route with special charecter we use regex
+
+// Route::get('parameter/{id?}' , function($id = ''){
+//     return $id;
+// })->where('id' , '[a-zA-Z0-9-&*]+');
+
+
+// if we want to put or owon parameter in route then we use optional && for retrection we use 
+// Route::get('parameter/{id?}' , function($id = ''){
+//     return $id;
+// })->whereIn('id' , ['Ahmad' , 'Ali' , 'hassan' ]);
